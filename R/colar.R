dbd3c27df70979ddef471070915c4ac836c00d2f
Alexandre Jaloto <alexandrejaloto@gmail.com>
2020-04-27 21:57:11 -0300

    Initial commit

---
+++ R/colar.R
@@ -0,0 +1,14 @@
+#' Colar dois vetores
+#'
+#' Cola dois vetores e mant??m um espa??o entre eles
+#'
+#' @param a primeiro vetor
+#' @param b segundo vetor
+#'
+#' @return Um novo vetor colado, com um espa??o entre eles
+#'
+#' @export
+
+colar = function (a, b) {
+  paste (a, b)
+}
